e978a2ebb8d2732b5628f3a9efde0b4254b79f0f
alexgreen <alex.green407@gmail.com>
2021-09-11 14:25:26 -0400

    beginning front end

--- src/index.js
+++ src/index.js
@@ -1,4 +1,43 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
+import { connect, Provider } from "react-redux";
+import store, { fetchStudents, fetchUniversities } from "./store";
+import Students from "./Students";
+import Universities from "./Universities";
 
-render(<hr />, document.querySelector('#root'));
+class _App extends Component {
+  componentDidMount() {
+    this.props.fetchStudents();
+    this.props.fetchUniversities();
+  }
+  render() {
+    const { students, universities } = this.props;
+    return (
+      <div>
+        <h1>Home</h1>
+        <div id="home">
+          <hr />
+          <Students />
+          <Universities />
+        </div>
+      </div>
+    );
+  }
+}
+
+const App = connect(
+  (state) => state,
+  (dispatch) => {
+    return {
+      fetchStudents: () => dispatch(fetchStudents()),
+      fetchUniversities: () => dispatch(fetchUniversities()),
+    };
+  }
+)(_App);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
---
+++ src/store.js
@@ -0,0 +1,68 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import axios from "axios";
+
+const LOAD_STUDENTS = "LOAD_STUDENTS";
+const CREATE_STUDENT = "CREATE_STUDENT";
+const DELETE_STUDENT = "DELETE_STUDENT";
+
+const LOAD_UNIVERSITIES = "LOAD_UNIVERSITIES";
+const CREATE_UNIVERSITY = "CREATE_UNIVERSITY";
+const DELETE_UNIVERSITY = "DELETE_UNIVERSITY";
+
+const students = (state = [], action) => {
+  //TODO make switch
+  if (action.type === "LOAD_STUDENTS") {
+    return action.students;
+  }
+  if (action.type === "CREATE_STUDENT") {
+    return [...state, action.student];
+  }
+  // if (action.type === "DELETE_STUDENT") {
+  //   return state.map((student) => (student.id === action.student.id ? action.student : student));
+  // }
+  return state;
+};
+
+const universities = (state = [], action) => {
+  //TODO make switch
+  if (action.type === "LOAD_UNIVERSITIES") {
+    return action.universities;
+  }
+  if (action.type === "CREATE_UNIVERSITY") {
+    return [...state, action.university];
+  }
+  // if (action.type === "DELETE_STUDENT") {
+  //   return state.map((university) => (university.id === action.university.id ? action.university : university));
+  // }
+  return state;
+};
+
+const store = createStore(
+  combineReducers({
+    students,
+    universities,
+  }),
+  applyMiddleware(thunk, logger)
+);
+
+export const fetchStudents = () => {
+  return async (dispatch) => {
+    const { data: students } = await axios.get("/api/students");
+    dispatch({
+      type: LOAD_STUDENTS,
+      students,
+    });
+  };
+};
+
+export const fetchUniversities = () => {
+  return async (dispatch) => {
+    const { data: universities } = await axios.get("/api/universities");
+    dispatch({
+      type: LOAD_UNIVERSITIES,
+      universities,
+    });
+  };
+};
